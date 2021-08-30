a887f838af782b0329e335ff1944a48facff41ed
swaminathanvasanth <vasanth.rajaraman@datakaveri.org>
2021-08-30 17:56:32 +0530

    Added db search functionality

--- src/main/java/iudx/gis/server/database/DatabaseServiceImpl.java
+++ src/main/java/iudx/gis/server/database/DatabaseServiceImpl.java
@@ -1,15 +1,39 @@
 package iudx.gis.server.database;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
 
 public class DatabaseServiceImpl implements DatabaseService {
 
+  private static final Logger LOGGER = LogManager.getLogger(DatabaseServiceImpl.class);
+  private PostgresClient pgSQLClient;
+  public static final String SELECT_GIS_SERVER_URL =
+      "SELECT * FROM gis WHERE iudx_resource_id='$1'";
+ 
+  public DatabaseServiceImpl(PostgresClient pgClient) {
+    // TODO Auto-generated constructor stub
+    this.pgSQLClient = pgClient;
+  }
+
   @Override
   public DatabaseService searchQuery(JsonObject request, Handler<AsyncResult<JsonObject>> handler) {
-    handler.handle(Future.succeededFuture(new JsonObject().put("a", "b")));
+    Future<JsonObject> getGISURL = getURLInDb(request.getString("id"));
+    getGISURL.onComplete(getUserApiKeyHandler -> {
+      if (getUserApiKeyHandler.succeeded()) {
+        LOGGER.info("DATABASE_READ_SUCCESS");
+        handler.handle(Future.succeededFuture(getUserApiKeyHandler.result()));
+      } else {
+        LOGGER.info("DATABASE_READ_FAILURE");
+      }
+    });
     return this;
   }
 
@@ -19,4 +43,38 @@ public class DatabaseServiceImpl implements DatabaseService {
     handler.handle(Future.succeededFuture(new JsonObject().put("a", "b")));
     return this;
   }
+
+  Future<JsonObject> getURLInDb(String id) {
+
+    LOGGER.debug("Info : PSQLClient#getUserInDb() started");
+    Promise<JsonObject> promise = Promise.promise();
+    JsonObject response = new JsonObject();
+    String query = SELECT_GIS_SERVER_URL.replace("$1", id);
+    LOGGER.debug("Info : " + query);
+    // Check in DB, get username and password
+    pgSQLClient.executeAsync(query).onComplete(db -> {
+      LOGGER.debug("Info : PSQLClient#getUserInDb()executeAsync completed");
+      if (db.succeeded()) {
+        LOGGER.debug("Info : PSQLClient#getUserInDb()executeAsync success");
+        String url = null;
+        // Get the apiKey
+        RowSet<Row> result = db.result();
+        if (db.result().size() > 0) {
+          for (Row row : result) {
+            url = row.getString(2);
+          }
+        }
+        response.put("type", "success");
+        response.put("title", "Successfully fetched the GIS server URL");
+        response.put("results", new JsonArray().add(new JsonObject().put("URL", url)));
+        promise.complete(response);
+      } else {
+        LOGGER.fatal("Fail : PSQLClient#getUserInDb()executeAsync failed");
+        promise.fail("Error : Get ID from database failed");
+      }
+    });
+    return promise.future();
+  }
+
+
 }
--- src/main/java/iudx/gis/server/database/DatabaseVerticle.java
+++ src/main/java/iudx/gis/server/database/DatabaseVerticle.java
@@ -3,7 +3,10 @@ package iudx.gis.server.database;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.json.JsonObject;
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.pgclient.PgPool;
 import io.vertx.serviceproxy.ServiceBinder;
+import io.vertx.sqlclient.PoolOptions;
 
 public class DatabaseVerticle extends AbstractVerticle {
   private DatabaseService database;
@@ -11,11 +14,46 @@ public class DatabaseVerticle extends AbstractVerticle {
   private ServiceBinder binder;
   private MessageConsumer<JsonObject> consumer;
 
+  /* Database Properties */
+  private String databaseIP;
+  private int databasePort;
+  private String databaseName;
+  private String databaseUserName;
+  private String databasePassword;
+  private int poolSize;
+  private PgPool pgclient;
+  private PoolOptions poolOptions;
+  private PgConnectOptions connectOptions;
+  private PostgresClient pgClient;
+  
   @Override
   public void start() throws Exception {
 
+    databaseIP = config().getString("databaseIP");
+    databasePort = config().getInteger("databasePort");
+    databaseName = config().getString("dbDatabaseName");
+    databaseUserName = config().getString("dbUser");
+    databasePassword = config().getString("dbPassword");
+    poolSize = config().getInteger("dbClientPoolSize");
+
+    /* Set Connection Object */
+    if (connectOptions == null) {
+      connectOptions = new PgConnectOptions().setPort(databasePort).setHost(databaseIP)
+          .setDatabase(databaseName).setUser(databaseUserName).setPassword(databasePassword);
+    }
+
+    /* Pool options */
+    if (poolOptions == null) {
+      poolOptions = new PoolOptions().setMaxSize(poolSize);
+    }
+
+    /* Create the client pool */
+    pgclient = PgPool.pool(vertx, connectOptions, poolOptions);
+
+    pgClient = new PostgresClient(vertx, connectOptions, poolOptions);
+
     binder = new ServiceBinder(vertx);
-    database = new DatabaseServiceImpl();
+    database = new DatabaseServiceImpl(pgClient);
     consumer =
         binder.setAddress(DATABASE_SERVICE_ADDRESS).register(DatabaseService.class, database);
   }
