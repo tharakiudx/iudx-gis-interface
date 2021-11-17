15adb419750e9afd64cbfbba90f23cf171f2b4a2
gautamv95 <gautam.verma@datakaveri.org>
2021-11-17 22:28:03 +0530

    add admin crud apis

--- src/main/java/iudx/gis/server/database/DatabaseServiceImpl.java
+++ src/main/java/iudx/gis/server/database/DatabaseServiceImpl.java
@@ -1,5 +1,6 @@
 package iudx.gis.server.database;
 
+import static iudx.gis.server.database.util.Constants.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import io.vertx.core.AsyncResult;
@@ -10,6 +11,8 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
+
+import java.util.Optional;
 
 public class DatabaseServiceImpl implements DatabaseService {
 
@@ -41,6 +44,94 @@ public class DatabaseServiceImpl implements DatabaseService {
   public DatabaseService insertIntoDb(JsonObject request,
       Handler<AsyncResult<JsonObject>> handler) {
     handler.handle(Future.succeededFuture(new JsonObject().put("a", "b")));
+    return this;
+  }
+
+  @Override
+  public DatabaseService insertAdminDetails(JsonObject request, Handler<AsyncResult<JsonObject>> handler) {
+    String resourceId = request.getString(ID);
+    String serverUrl = request.getString(SERVER_URL);
+    Long serverPort = request.getLong(SERVER_PORT);
+    Boolean isSecure = request.getBoolean(SECURE);
+
+    Optional<JsonObject> accessInfo = Optional.ofNullable(request.getJsonObject(ACCESS_INFO));
+
+    String query = INSERT_ADMIN_DETAILS_QUERY.replace("$1", resourceId)
+        .replace("$2", serverUrl)
+        .replace("$3", serverPort.toString())
+        .replace("$4", isSecure.toString());
+
+    if (accessInfo.isPresent() && !accessInfo.get().isEmpty()) {
+      JsonObject accessObject = accessInfo.get();
+      String username = accessObject.getString(USERNAME);
+      String password = accessObject.getString(PASSWORD);
+      query = query.replace("$5", username).replace("$6", password);
+    } else {
+      query = query.replace("$5", "").replace("$6", "");
+    }
+
+    pgSQLClient.executeAsync(query)
+        .onSuccess(ar -> {
+          LOGGER.debug("Insert admin details operation successful!");
+          handler.handle(Future.succeededFuture(new JsonObject().put(TYPE, SUCCESS)));
+        })
+        .onFailure(ar -> {
+          LOGGER.error("Insert admin operation failed due to: {}", ar.toString());
+          handler.handle(Future.failedFuture(ar));
+        });
+    return this;
+  }
+
+  @Override
+  public DatabaseService updateAdminDetails(JsonObject request, Handler<AsyncResult<JsonObject>> handler) {
+    String resourceId = request.getString(ID);
+    String serverUrl = request.getString(SERVER_URL);
+    Long serverPort = request.getLong(SERVER_PORT);
+    Boolean isSecure = request.getBoolean(SECURE);
+
+    Optional<JsonObject> accessInfo = Optional.ofNullable(request.getJsonObject(ACCESS_INFO));
+
+    String query = UPDATE_ADMIN_DETAILS_QUERY.replace("$1", serverUrl)
+        .replace("$2", serverPort.toString())
+        .replace("$3", isSecure.toString())
+        .replace("$6", resourceId);
+
+    if (accessInfo.isPresent() && !accessInfo.get().isEmpty()) {
+      JsonObject accessObject = accessInfo.get();
+      String username = accessObject.getString(USERNAME);
+      String password = accessObject.getString(PASSWORD);
+      query = query.replace("$4", username).replace("$5", password);
+    } else {
+      query = query.replace("$4", "").replace("$5", "");
+    }
+
+    pgSQLClient.executeAsync(query)
+        .onSuccess(ar -> {
+          LOGGER.debug("Update admin details operation successful!");
+          handler.handle(Future.succeededFuture(new JsonObject().put(TYPE, SUCCESS)));
+        })
+        .onFailure(ar -> {
+          LOGGER.error("Update admin operation failed due to: {}", ar.toString());
+          handler.handle(Future.failedFuture(ar));
+        });
+
+    return this;
+  }
+
+  @Override
+  public DatabaseService deleteAdminDetails(String resourceId, Handler<AsyncResult<JsonObject>> handler) {
+    String query = DELETE_ADMIN_DETAILS_QUERY.replace("$1", resourceId);
+
+    pgSQLClient.executeAsync(query)
+        .onSuccess(ar -> {
+          LOGGER.debug("Delete admin details operation successful!");
+          handler.handle(Future.succeededFuture(new JsonObject().put(TYPE, SUCCESS)));
+        })
+        .onFailure(ar -> {
+          LOGGER.error("Delete admin operation failed due to: {}", ar.toString());
+          handler.handle(Future.failedFuture(ar));
+        });
+
     return this;
   }
 
