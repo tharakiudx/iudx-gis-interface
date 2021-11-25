239a113bd065257a0361920dc2fa26fb1769edfb
gautamv95 <gautam.verma@datakaveri.org>
2021-11-25 21:48:51 +0530

    added urns

---
+++ src/main/java/iudx/gis/server/apiserver/response/ResponseUrn.java
@@ -0,0 +1,53 @@
+package iudx.gis.server.apiserver.response;
+
+import java.util.stream.Stream;
+
+public enum ResponseUrn {
+  SUCCESS("urn:dx:rs:success", "Successful operation"),
+  INVALID_ATTR_PARAM("urn:dx:rs:invalidAttributeParam", "Invalid attribute param"),
+  INVALID_ATTR_VALUE("urn:dx:rs:invalidAttributeValue", "Invalid attribute value"),
+  INVALID_OPERATION("urn:dx:rs:invalidOperation", "Invalid operation"),
+  UNAUTHORIZED_ENDPOINT("urn:dx:rs:unauthorizedEndpoint", "Access to endpoint is not available"),
+  UNAUTHORIZED_RESOURCE("urn,dx:rs:unauthorizedResource", "Access to resource is not available"),
+  EXPIRED_TOKEN("urn:dx:rs:expiredAuthorizationToken", "Token has expired"),
+  MISSING_TOKEN("urn:dx:rs:missingAuthorizationToken", "Token needed and not present"),
+  INVALID_TOKEN("urn:dx:rs:invalidAuthorizationToken", "Token is invalid"),
+
+  RESOURCE_NOT_FOUND("urn:dx:rs:resourceNotFound", "Document of given id does not exist"),
+  METHOD_NOT_FOUND("urn:dx:rs:MethodNotAllowed", "Method not allowed for given endpoint"),
+  UNSUPPORTED_MEDIA_TYPE("urn:dx:rs:UnsupportedMediaType", "Requested/Presented media type not supported"),
+
+  RESPONSE_PAYLOAD_EXCEED("urn:dx:rs:responsePayloadLimitExceeded", "Search operations exceeds the default response payload limit"),
+  REQUEST_PAYLOAD_EXCEED("urn:dx:rs:requestPayloadLimitExceeded", "Operation exceeds the default request payload limit"),
+  REQUEST_OFFSET_EXCEED("urn:dx:rs:requestOffsetLimitExceeded", "Operation exceeds the default value of offset"),
+  REQUEST_LIMIT_EXCEED("urn:dx:rs:requestLimitExceeded", "Operation exceeds the default value of limit"),
+
+  YET_NOT_IMPLEMENTED("urn:dx:rs:general", "urn yet not implemented in backend verticle.");
+
+  private final String urn;
+  private final String message;
+
+  ResponseUrn(String urn, String message) {
+    this.urn = urn;
+    this.message = message;
+  }
+
+  public String getUrn() {
+    return urn;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public static ResponseUrn fromCode(final String urn) {
+    return Stream.of(values())
+        .filter(v -> v.urn.equalsIgnoreCase(urn))
+        .findAny()
+        .orElse(YET_NOT_IMPLEMENTED); /* If backend services don't respond with URN */
+  }
+
+  public String toString() {
+    return "[" + urn + " : " + message + " ]";
+  }
+}
