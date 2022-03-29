9fb8183d65a249ab5e3133b6e76c9e3b7baeee6b
Gopal Mahajan <60537181+gopal-mahajan@users.noreply.github.com>
2022-03-29 09:54:27 +0530

    Update Constants.java

--- src/main/java/iudx/gis/server/common/Constants.java
+++ src/main/java/iudx/gis/server/common/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
   public static final String AUTHENTICATION_SERVICE_ADDRESS = "iudx.gis.authentication.service";
   public static final String DATABASE_SERVICE_ADDRESS = "iudx.gis.database.service";
   public static final String DATABROKER_SERVICE_ADDRESS = "iudx.gis.broker.service";
+  public static final String GIS_INVALID_SUB="invalid-tokens";
 
   // postgres queries
   public static String SELECT_REVOKE_TOKEN_SQL = "SELECT * FROM revoked_tokens";
