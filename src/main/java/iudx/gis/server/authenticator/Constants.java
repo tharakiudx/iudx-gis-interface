4c3734fb7a39c46d910884d1f5b613fef092a0e5
Kailash Adhikari <adhikari.kailash88@gmail.com>
2023-03-03 13:23:53 +0530

    constant value changed

--- src/main/java/iudx/gis/server/authenticator/Constants.java
+++ src/main/java/iudx/gis/server/authenticator/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
   public static final List<String> OPEN_ENDPOINTS = List.of("/entities");
   public static final long CACHE_TIMEOUT_AMOUNT = 30;
   public static final String CAT_ITEM_PATH = "/item";
-  public static final String CAT_SEARCH_PATH = "/iudx/cat/v1/search";
+  public static final String CAT_SEARCH_PATH = "/search";
 
   public static final String AUTH_CERTIFICATE_PATH = "/cert";
   public static final String JSON_USERID = "userid";
